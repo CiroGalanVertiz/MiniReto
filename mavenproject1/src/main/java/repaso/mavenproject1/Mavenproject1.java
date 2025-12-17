0c38c5692ef5eba804a86f89b17eb1c2eb524667
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2025-12-17 18:46:24 +0100

    a

---
+++ mavenproject1/src/main/java/repaso/mavenproject1/Mavenproject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package repaso.mavenproject1;
+
+/**
+ *
+ * @author damloe05
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        Juego juego = new Juego();
+        juego.iniciar();
+    }
+}
